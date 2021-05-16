87ed232113d3cfb7f2fd9f041e3caa0466980609
Osaretin Idele <ideleosaretin@yahoo.com>
2021-05-16 10:13:17 +0300

    added backendAPI class

---
+++ FirebaseBackendAPI/BackendAPI.swift
@@ -0,0 +1,12 @@
+//
+//  BackendAPI.swift
+//  FirebaseBackendAPI
+//
+//  Created by Osaretin Idele on 16/05/2021.
+//
+
+import UIKit
+
+public class BackendAPI<T : Codable>: NSObject, Codable {
+
+}
